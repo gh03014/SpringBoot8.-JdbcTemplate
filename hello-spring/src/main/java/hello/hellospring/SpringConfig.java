e81258b29a44b6c151ec3c35c070b0b0e9c1178b
gh03014 <jaehwan3399@gmail.com>
2021-01-06 22:47:16 +0900

    스프링부트_JdbcTemplate

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,32 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration //자바 코드로 스프링 빈 등록
+public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    
+    @Bean //bean 객체 생성, 등록
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+}
